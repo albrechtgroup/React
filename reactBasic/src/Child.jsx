7f15fa7f5ade44461d526faaecbf28c81d672341
Andy Albrecht <aalbrecht33733@gmail.com>
2023-05-07 20:02:51 -0400

    Renaming React.js Basic App

---
+++ reactBasic/src/Child.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Child(props) {
+    return (
+        <div>
+            <h2>Child Component</h2>
+            <h3>{props.userInput}</h3>
+        </div>
+    )
+}
+
+export default Child
+
